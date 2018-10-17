3310b08f8db5156a60557b7c9d1bbc20913193e8
Mert Batmazoglu <mertbatmazoglu@gmail.com>
2018-10-17 10:50:16 +0300

    refactored

--- index.js
+++ index.js
@@ -14,46 +14,36 @@
 			if (child.tagName === "INPUT") {
 				// check for the conditions
 				const splittedName = child.name.split(".");
-				// check for array
-				if (splittedName.length < 2 && splittedName[0].includes("[]")) {
-					// add the value to the array
-					const newArr = splittedName[0].slice(0, splittedName[0].length - 2);
-					// check for the existence
-					if(returnedObj[newArr]) {
-						returnedObj[newArr].push(child.value);
-					} else {
-						returnedObj[newArr] = [child.value];
+				let nameChain = returnedObj;
+				// loop through the splittedName
+				splittedName.forEach((name, index) => {
+					// if its the last element
+					if (index === splittedName.length - 1) {
+						// check the last element if it contains brackets
+						if (name.includes("[]")) {
+							// check the previous 
+							name = name.slice(0, name.length - 2);
+							if (nameChain[name]) {
+								nameChain[name].push(child.value);
+							} else {
+								nameChain[name] = [child.value];
+							}
+						} else {
+							// if not array
+							nameChain[name] = child.value;	
+						}
+						
+						return;
 					}
-					return;
-				}
-
-				if(splittedName.length === 2 && splittedName[1].includes("[]")) {
-
-					const newArr = splittedName[1].slice(0, splittedName[1].length - 2);
-					const newObj = splittedName[0];
-					// check again
-					if(returnedObj[newObj] && returnedObj[newObj][newArr]) {
-						returnedObj[newObj][newArr].push(child.value);	
-					} else {
-						returnedObj[newObj] = { };
-						returnedObj[newObj][newArr] = [child.value];
-					}
-					return;
-				}
-				
-				if(splittedName.length === 2 && !splittedName[1].includes("[]")) {
-					// if there is no brackets use object notation
-					returnedObj[splittedName[0]][splittedName[1]] = child.value;
-					return;
-				}
-				// if there is no brackets or linked objects
-				else {
-					returnedObj[splittedName[0]] = child.value;
-				}				
+					// if not continue
+					nameChain[name] = {};
+					nameChain = nameChain[name];
+				});				
 			}
 		});
 		return returnedObj;
 	}
+	
 	formToObj.init = init;
 	window.formToObj = formToObj;
 
